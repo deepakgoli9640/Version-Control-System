efc75f089bcb09e88b8e5a18b544d18c41bd6070
deepakgoli9640 <deepakgoli9640@gmail.com>
2025-07-27 16:21:36 +0530

    final commit

--- src/main/java/org/vcs/Main.java
+++ src/main/java/org/vcs/Main.java
@@ -1,18 +1,57 @@
 package org.vcs;
 
+import org.vcs.Groot;
+
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.security.NoSuchAlgorithmException;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
-        Groot groot=new Groot(Path.of("."));
-       groot.add(String.valueOf(Paths.get("sample.txt")));
-       groot.add(String.valueOf(Paths.get("deepak.txt")));
-       groot.commit("Third commit");
-       groot.log();
+        if (args.length == 0) {
+            printUsage();
+            return;
+        }
+
+        Groot groot = new Groot(Path.of("."));
+        String command = args[0];
+
+        switch (command) {
+            case "add":
+                if (args.length < 2) {
+                    System.out.println("❌ Please specify at least one file to add.");
+                } else {
+                    for (int i = 1; i < args.length; i++) {
+                        groot.add(args[i]);
+                        System.out.println("✅ Added: " + args[i]);
+                    }
+                }
+                break;
+
+            case "commit":
+                if (args.length < 2) {
+                    System.out.println("❌ Please provide a commit message.");
+                } else {
+                    String message = args[1];
+                    groot.commit(message);
+                    System.out.println("✅ Commit complete: " + message);
+                }
+                break;
+
+            case "log":
+                groot.log();
+                break;
+
+            default:
+                System.out.println("❌ Unknown command: " + command);
+                printUsage();
         }
     }
+
+    private static void printUsage() {
+        System.out.println("Usage:");
+        System.out.println("  groot add <file1> <file2> ...");
+        System.out.println("  groot commit \"your message\"");
+        System.out.println("  groot log");
+    }
+}
